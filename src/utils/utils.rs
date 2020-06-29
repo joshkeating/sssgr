a28866d5aecdc9775bfb07de20c77baed99277da
joshkeating <josh.keating7@gmail.com>
2020-06-28 21:43:19 -0700

    fix the date string to int so that it correctly weights days < months < years

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,18 +1,24 @@
 
+// an overly complex way to not deal with importing the chrono lib
+// weights days < months < years
 pub fn date_str_to_int(date: &str) -> i32 {
 
     assert_eq!(date.len(), 10, "provided str length is not 10");
 
     let mut total: i32 = 0;
-    let split_date: Vec<&str> = date.split('/').collect();
+    let mut split_date: Vec<&str> = date.split('/').collect();
 
-    for n in split_date {
-        // if first char of str is 0, only add second char
-        if &n[0..1] == "0" && n.len() == 2 {
-            let second_char = &n[1..2];
-            total += second_char.parse::<i32>().unwrap();
+    for i in 0..3 {
+        let mut segment = split_date.pop().unwrap();
+        if &segment[0..1] == "0" && segment.len() == 2 {
+            segment = &segment[1..2];
+        }
+        if i == 2 {
+            total += segment.parse::<i32>().unwrap() * 100
+        } else if i == 0 {
+            total += segment.parse::<i32>().unwrap() * 1000
         } else {
-            total += n.parse::<i32>().unwrap();
+            total += segment.parse::<i32>().unwrap();
         }
     }
     total
@@ -24,8 +30,10 @@ mod tests {
 
     #[test]
     fn test_date_str_to_int() {
-        assert_eq!(date_str_to_int("10/09/2017"), 2036);
-        assert_eq!(date_str_to_int("01/13/1997"), 2011);
+        assert_eq!(date_str_to_int("10/09/2017"), 2018009);
+        assert_eq!(date_str_to_int("01/13/1997"), 1997113);
+        assert_eq!(date_str_to_int("12/19/2016"), 2017219);
+        assert_eq!(date_str_to_int("12/04/2018"), 2019204);
     }
 
     #[test]
